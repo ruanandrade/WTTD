e744aa524ca24f995bbd711de165f50f6253953e
ruan <ruan.andrade@funcate.org.br>
2017-10-16 14:07:12 -0200

    exercises answers - list1.py.

--- google-python-exercises/basic/list1.py
+++ google-python-exercises/basic/list1.py
@@ -22,8 +22,11 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-    # +++your code here+++
-    return
+    count = 0
+    for word in words:
+        if (len(word) > 1) and (word[0] == word[-1]):
+            count += 1
+    return count
 
 
 # B. front_x
@@ -34,8 +37,12 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    # +++your code here+++
-    return
+    xwords = []
+    for word in words[::-1]:
+        if word[0] == 'x':
+            xwords.append(word)
+            words.remove(word)
+    return sorted(xwords) + sorted(words)
 
 
 # C. sort_last
@@ -45,9 +52,10 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    # +++your code here+++
-    return
+    return sorted(tuples, key=last)
 
+def last(t):
+    return t[-1]
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
